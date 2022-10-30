4add559ca0620b4cba2fb4621fe54e3feeb628f2
Li-Huai (Allan) Lin <qqaatw@gmail.com>
2022-10-30 19:02:00 +0800

    Update CI/CD

--- build_wheels.sh
+++ build_wheels.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 set -e -u -x
 
-repair_wheel () {
-    wheel="$1"
-    if ! auditwheel show "$wheel"; then
-        echo "Skipping non-platform wheel $wheel"
-    else
-        auditwheel repair "$wheel" --plat "$PLAT" -w wheelhouse/
-    fi
-}
-
-
-#cd /io
-
 if [ ! -d aws-crt-python ] 
 then
     git clone https://github.com/awslabs/aws-crt-python.git --recursive
@@ -21,21 +9,6 @@ fi
 # Check out and update latest version
 cd aws-crt-python/
 git checkout $(git tag | sort -V | tail -1)
-/opt/python/cp310-cp310/bin/python ./continuous-delivery/update-version.py
-cd ..
+python ./continuous-delivery/update-version.py
 
-# Compile wheels
-for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" wheel aws-crt-python/ --no-deps -w wheelhouse/
-done
-
-# Bundle external shared libraries into the wheels
-for whl in wheelhouse/*.whl; do
-    repair_wheel "$whl"
-done
-
-# Install packages and test
-for PYBIN in /opt/python/*/bin/; do
-    "${PYBIN}/pip" install awscrt --no-index -f wheelhouse
-    (cd aws-crt-python/; "${PYBIN}python" -m unittest discover --failfast --verbose)
-done
+python -m cibuildwheel --output-dir "../wheelhouse"
---
+++ check_and_update.py
@@ -0,0 +1,41 @@
+import requests
+
+from argparse import ArgumentParser
+
+GITHUB_UPSTREAM_RELEASE_ENDPOINT = "https://api.github.com/repos/awslabs/aws-crt-python/releases"
+GITHUB_RELEASE_ENDPOINT = "https://api.github.com/repos/qqaatw/aws-crt-python-musllinux/releases"
+
+def check():
+    latest_upstream_release_tag = requests.get(GITHUB_UPSTREAM_RELEASE_ENDPOINT).json()[0]["tag_name"].replace("v","")
+    latest_release_tag = requests.get(GITHUB_RELEASE_ENDPOINT).json()[0]["tag_name"]
+
+    if latest_upstream_release_tag == latest_release_tag:
+        print(f"LATEST=true;UPSTREAM={latest_upstream_release_tag};MAIN={latest_release_tag}")
+    else:
+        print(f"LATEST=false;UPSTREAM={latest_upstream_release_tag};MAIN={latest_release_tag}")
+
+def update():
+    template ="""<!DOCTYPE html><html><body>{}</body></html>"""
+
+    releases = requests.get(GITHUB_RELEASE_ENDPOINT).json()
+    asset_list = []
+
+    for release in releases:
+        for asset in release["assets"]:
+            asset_list.append(f'<a href="{asset["browser_download_url"]}">{asset["name"]}</a>')
+
+    with open("index.html", "w", encoding="utf8") as f:
+        f.write(template.format("<br>".join(asset_list)))
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    
+    parser.add_argument("--task", choices=["check", "update"], required=True)
+
+    args = parser.parse_args()
+
+    if args.task == "check":
+        check()
+    elif args.task == "update":
+        update()
